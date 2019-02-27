869092089c0b52142734142eb17a90d5e5353f48
Mike Ellertson <mdellertson@gmail.com>
2019-02-27 15:39:12 -0700

    tests pass in Jaden Casing Strings

--- codewars/jadencasingstrings.py
+++ codewars/jadencasingstrings.py
@@ -6,6 +6,15 @@ __all__ = [
 
 
 def toJadenCase(string):
+	"""
+	Convert a sentence to have correct case for each word
+
+	:param string: An improperly cased sentence.
+	:type string: str
+	:return: The sentence with corrected case for each word.
+	:rtype: str
+	"""
+
 	jcs = string.split(' ')
 	for i in range(len(jcs)):
 		jcs[i] = jcs[i][0].upper() + jcs[i][1:]
