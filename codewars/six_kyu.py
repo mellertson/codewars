5373c72e030062ab718032d5a6464b2b062d3185
Mike Ellertson <mdellertson@gmail.com>
2019-02-27 17:50:01 -0700

    beginning balance challenge

---
+++ codewars/six_kyu.py
@@ -0,0 +1,18 @@
+
+
+__all__ = [
+	'balance',
+]
+
+
+def balance(book):
+    """
+	Parse lines from a checkbook into a report
+
+    :param book: The lines of the checkbook
+    :type book: str
+    :return: The lines of the report
+    :rtype: str
+    """
+
+
