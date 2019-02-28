0007b4c6780479d55493309a95924156635bec23
Mike Ellertson <mdellertson@gmail.com>
2019-02-27 18:59:05 -0700

    all tests passing in balance challenge

--- codewars/six_kyu.py
+++ codewars/six_kyu.py
@@ -1,3 +1,4 @@
+import re, copy
 
 
 __all__ = [
@@ -5,14 +6,45 @@ __all__ = [
 ]
 
 
+invalid_ptn = re.compile(r'[^a-zA-Z\d .]')
+check_n_ptn = re.compile(r'(\d+)')
+category_ptn = re.compile(r'([a-zA-Z]+)')
+amount_ptn = re.compile(r'(\d+[.]\d+)')
+
+
 def balance(book):
-    """
+	"""
 	Parse lines from a checkbook into a report
 
-    :param book: The lines of the checkbook
-    :type book: str
-    :return: The lines of the report
-    :rtype: str
-    """
+	:param book: The lines of the checkbook
+	:type book: str
+	:return: The lines of the report
+	:rtype: str
+	"""
 
+	global invalid_ptn, check_n_ptn, category_ptn
+	ilines = book.split('\n')
+	olines = []
+	m = amount_ptn.match(ilines[0])
+	starting_bal = float(m.group(1)) if m else 0.0
+	olines.append(f'Original Balance: {starting_bal:.2f}')
+	amounts = []
+	for line in ilines[1:]:
+		if len(line) == 0:
+			continue
+		line = re.sub(invalid_ptn, '', line)
+		check_match = check_n_ptn.match(line)
+		check_n = check_match.group(1) if check_match else None
+		category_match = category_ptn.search(line)
+		category = category_match.group(1) if category_match else None
+		amount_match = amount_ptn.search(line)
+		amount = float(amount_match.group(1)) if amount_match else 0.0
+		amount = int(amount * 100) / 100.0
+		amounts.append(amount)
+
+		# store the output line
+		olines.append(f'{check_n} {category} {amount:.2f} Balance {starting_bal - sum(amounts):.2f}')
+	olines.append(f'Total expense  {sum(amounts):.2f}')
+	olines.append(f'Average expense  {sum(amounts) / len(amounts):.2f}')
+	return '\r\n'.join(olines)
 
--- codewars/tests/test_six_kyu.py
+++ codewars/tests/test_six_kyu.py
@@ -4,88 +4,90 @@ from codewars.six_kyu import *
 
 class balance_Tests(unittest.TestCase):
 
-	def test_should_pass(self):
+	def test_should_pass_b1(self):
 		b1 = """1000.00!=
 
-		125 Market !=:125.45
-		126 Hardware =34.95
-		127 Video! 7.45
-		128 Book :14.32
-		129 Gasoline ::16.10
-		"""
+125 Market !=:125.45
+126 Hardware =34.95
+127 Video! 7.45
+128 Book :14.32
+129 Gasoline ::16.10
+"""
 
 		b1sol = """Original Balance: 1000.00\r
-		125 Market 125.45 Balance 874.55\r
-		126 Hardware 34.95 Balance 839.60\r
-		127 Video 7.45 Balance 832.15\r
-		128 Book 14.32 Balance 817.83\r
-		129 Gasoline 16.10 Balance 801.73\r
-		Total expense  198.27\r
-		Average expense  39.65"""
+125 Market 125.45 Balance 874.55\r
+126 Hardware 34.95 Balance 839.60\r
+127 Video 7.45 Balance 832.15\r
+128 Book 14.32 Balance 817.83\r
+129 Gasoline 16.10 Balance 801.73\r
+Total expense  198.27\r
+Average expense  39.65"""
 
 		self.assertEqual(b1sol, balance(b1))
 
+	def test_should_pass_b2(self):
 		b2 = """1233.00
-		125 Hardware;! 24.8?;
-		123 Flowers 93.5
-		127 Meat 120.90
-		120 Picture 34.00
-		124 Gasoline 11.00
-		123 Photos;! 71.4?;
-		122 Picture 93.5
-		132 Tyres;! 19.00,?;
-		129 Stamps 13.6
-		129 Fruits{} 17.6
-		129 Market;! 128.00?;
-		121 Gasoline;! 13.6?;"""
+125 Hardware;! 24.8?;
+123 Flowers 93.5
+127 Meat 120.90
+120 Picture 34.00
+124 Gasoline 11.00
+123 Photos;! 71.4?;
+122 Picture 93.5
+132 Tyres;! 19.00,?;
+129 Stamps 13.6
+129 Fruits{} 17.6
+129 Market;! 128.00?;
+121 Gasoline;! 13.6?;"""
 
 		b2sol = """Original Balance: 1233.00\r
-		125 Hardware 24.80 Balance 1208.20\r
-		123 Flowers 93.50 Balance 1114.70\r
-		127 Meat 120.90 Balance 993.80\r
-		120 Picture 34.00 Balance 959.80\r
-		124 Gasoline 11.00 Balance 948.80\r
-		123 Photos 71.40 Balance 877.40\r
-		122 Picture 93.50 Balance 783.90\r
-		132 Tyres 19.00 Balance 764.90\r
-		129 Stamps 13.60 Balance 751.30\r
-		129 Fruits 17.60 Balance 733.70\r
-		129 Market 128.00 Balance 605.70\r
-		121 Gasoline 13.60 Balance 592.10\r
-		Total expense  640.90\r
-		Average expense  53.41"""
+125 Hardware 24.80 Balance 1208.20\r
+123 Flowers 93.50 Balance 1114.70\r
+127 Meat 120.90 Balance 993.80\r
+120 Picture 34.00 Balance 959.80\r
+124 Gasoline 11.00 Balance 948.80\r
+123 Photos 71.40 Balance 877.40\r
+122 Picture 93.50 Balance 783.90\r
+132 Tyres 19.00 Balance 764.90\r
+129 Stamps 13.60 Balance 751.30\r
+129 Fruits 17.60 Balance 733.70\r
+129 Market 128.00 Balance 605.70\r
+121 Gasoline 13.60 Balance 592.10\r
+Total expense  640.90\r
+Average expense  53.41"""
 
 		self.assertEqual(b2sol, balance(b2))
 
+	def test_should_pass_b3(self):
 		b3 = """1233.00
-		125 Hardware;! 24.801?;
-		123 Flowers 93.5025
-		127 Meat 120.9034
-		120 Picture 34.00
-		124 Gasoline 11.00
-		123 Photos;! 71.4?;
-		122 Picture 93.5
-		132 Tyres;! 19.003,?;
-		129 Stamps 13.6
-		129 Fruits{} 17.6
-		129 Market;! 128.001?;
-		121 Gasoline;! 13.6028?;"""
+125 Hardware;!  24.801?;
+123 Flowers 93.5025
+127 Meat 120.9034
+120 Picture 34.00
+124 Gasoline 11.00
+123 Photos;! 71.4?;
+122 Picture 93.5
+132 Tyres;! 19.003,?;
+129 Stamps 13.6
+129 Fruits{} 17.6
+129 Market;! 128.001?;
+121 Gasoline;! 13.6028?;"""
 
 		b3sol = """Original Balance: 1233.00\r
-		125 Hardware 24.80 Balance 1208.20\r
-		123 Flowers 93.50 Balance 1114.70\r
-		127 Meat 120.90 Balance 993.80\r
-		120 Picture 34.00 Balance 959.80\r
-		124 Gasoline 11.00 Balance 948.80\r
-		123 Photos 71.40 Balance 877.40\r
-		122 Picture 93.50 Balance 783.90\r
-		132 Tyres 19.00 Balance 764.90\r
-		129 Stamps 13.60 Balance 751.30\r
-		129 Fruits 17.60 Balance 733.70\r
-		129 Market 128.00 Balance 605.70\r
-		121 Gasoline 13.60 Balance 592.10\r
-		Total expense  640.90\r
-		Average expense  53.41"""
+125 Hardware 24.80 Balance 1208.20\r
+123 Flowers 93.50 Balance 1114.70\r
+127 Meat 120.90 Balance 993.80\r
+120 Picture 34.00 Balance 959.80\r
+124 Gasoline 11.00 Balance 948.80\r
+123 Photos 71.40 Balance 877.40\r
+122 Picture 93.50 Balance 783.90\r
+132 Tyres 19.00 Balance 764.90\r
+129 Stamps 13.60 Balance 751.30\r
+129 Fruits 17.60 Balance 733.70\r
+129 Market 128.00 Balance 605.70\r
+121 Gasoline 13.60 Balance 592.10\r
+Total expense  640.90\r
+Average expense  53.41"""
 
 		self.assertEqual(b3sol, balance(b3))
 
